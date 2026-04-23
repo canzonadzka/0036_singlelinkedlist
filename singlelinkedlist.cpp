22aa9f04a48669ef52d28e404f1540ce17594159
canzonadzka <canzonadzka@gmail.com>
2026-04-23 15:45:15 +0700

    menambahkan node previous dan current

--- singlelinkedlist.cpp
+++ singlelinkedlist.cpp
@@ -9,12 +9,12 @@ public:
     Node *next;
 };
 
-class Linkedlist
+class LinkedList   
 {
     Node *START;
 
 public:
-    LinkedList()
+    LinkedList()   
     {
         START = NULL;
     }
@@ -27,7 +27,37 @@ public:
 
         Node *nodeBaru = new Node;
         nodeBaru->noMhs = nim;
-    }
-    
-}
+        nodeBaru->next = NULL;
 
+    
+        if (START == NULL || nim <= START->noMhs)
+        {
+            if (START != NULL && nim == START->noMhs)
+            {
+                cout << "\nDuplikasi noMhs tidak diijinkan\n";
+                return;
+            }
+
+            nodeBaru->next = START;
+            START = nodeBaru;
+            return;
+        }
+
+        Node *previous = START;
+        Node *current = START;
+
+        while (current != NULL && nim > current->noMhs)
+        {
+            if (nim == current->noMhs)
+            {
+                cout << "\nDuplikasi noMhs tidak diijinkan\n";
+                return;
+            }
+            previous = current;
+            current = current->next;
+        }
+
+        nodeBaru->next = current;
+        previous->next = nodeBaru;
+    }
+};
